86d636ddcfc066634dfb3f0854d728b070d05a3f
paul wisehart <paul@oldcode.org>
2018-07-11 07:45:11 -0400

    try for DS3234

--- blink2.ino
+++ blink2.ino
@@ -1,39 +1,171 @@
-/* @(#) $Id: blink2.ino 34 2017-03-19 17:51:19Z leres $ (XSE) */
+#include <SPI.h>
 
 /*
- * This is a example sketch that blinks the LED like a heartbeat
+ * 1 - Setup the clock such that we get a SQW output 2 - Set the time of the
+ * clock 3 - Read the time of the clock
  */
 
-#ifndef PIN_LED_OUT
-#define PIN_LED_OUT LED_BUILTIN
-#endif
+int		chipSelect = 8;
+int		intFreq = 4;
 
-#ifndef LED_ON
-#define LED_ON HIGH
-#endif
+#define WRITE_CONTROL_REG 0x8E
+#define READ_CONTROL_REG 0x0E
+#define WRITE_TIME_REG 0x80
+#define READ_TIME_REG 0x00
 
-#ifndef LED_OFF
-#define LED_OFF LOW
-#endif
+typedef struct {
+	uint8_t		ss;
+	uint8_t		mm;
+	uint8_t		hh;
+	uint8_t		dy;
+	uint8_t		d;
+	uint8_t		m;
+	uint8_t		y;
+}		timeParameters;
 
-void
-setup()
+
+void 
+RTC_init(int chipSelect, int intFreq)
 {
-	pinMode(PIN_LED_OUT, OUTPUT);
+	/*
+	 * The DS3234 offers four output frequencies: Options: 1:     1
+	 * Hz 2:     1.024 kHz 3:     4.096 kHz 4:     8.192 kHz 5:     OFF
+	 */
+
+	pinMode(chipSelect, OUTPUT);
+
+	digitalWrite(chipSelect, LOW);
+	SPI.transfer(READ_CONTROL_REG);
+	byte		originalConfig = SPI.transfer(0x00);
+	digitalWrite(chipSelect, HIGH);
+	delay(10);
+
+	byte		configModifier;
+	byte		newConfig;
+
+	if (intFreq == 5) {
+		configModifier = 0 b10111111;
+		newConfig = configModifier & originalConfig;
+	} else if (intFreq < 5) {
+		uint8_t		freqOption = intFreq - 1;
+		configModifier = (freqOption << 3) | 0 b01000000;
+		newConfig = configModifier | (originalConfig & 0 b11100011);
+	} else {
+		newConfig = 0 b11100011;
+	}
+
+	digitalWrite(chipSelect, LOW);
+	SPI.transfer(WRITE_CONTROL_REG);
+	SPI.transfer(newConfig);
+	digitalWrite(chipSelect, HIGH);
+	delay(10);
 }
 
-void
+static uint8_t 
+convertValueIN(uint8_t value)
+{
+	uint8_t		convertedVal = value - 6 * (value >> 4);
+	return convertedVal;
+}
+
+static uint8_t 
+convertValueOUT(uint8_t value)
+{
+	uint8_t		convertedVal = value + 6 * (value / 10);
+	return convertedVal;
+}
+
+void 
+setTime(int chipSelect, timeParameters * timeVals)
+{
+	pinMode(chipSelect, OUTPUT);
+
+	digitalWrite(chipSelect, LOW);
+	SPI.transfer(WRITE_TIME_REG);
+	SPI.transfer(convertValueOUT(timeVals->ss));
+	SPI.transfer(convertValueOUT(timeVals->mm));
+	SPI.transfer(convertValueOUT(timeVals->hh));
+	SPI.transfer(convertValueOUT(timeVals->dy));
+	SPI.transfer(convertValueOUT(timeVals->d));
+	SPI.transfer(convertValueOUT(timeVals->m));
+	SPI.transfer(convertValueOUT(timeVals->y));
+	digitalWrite(chipSelect, HIGH);
+	delay(10);
+
+}
+
+void 
+readTime(int chipSelect, timeParameters * timeVals)
+{
+	pinMode(chipSelect, OUTPUT);
+
+	digitalWrite(chipSelect, LOW);
+	SPI.transfer(READ_TIME_REG);
+	timeVals->ss = convertValueIN(SPI.transfer(0x00));
+	timeVals->mm = convertValueIN(SPI.transfer(0x00));
+	timeVals->hh = convertValueIN(SPI.transfer(0x00));
+	timeVals->dy = convertValueIN(SPI.transfer(0x00));
+	timeVals->d = convertValueIN(SPI.transfer(0x00));
+	timeVals->m = convertValueIN(SPI.transfer(0x00));
+	timeVals->y = convertValueIN(SPI.transfer(0x00));
+	digitalWrite(chipSelect, HIGH);
+	delay(10);
+}
+
+timeParameters	example_vals = {
+	50,
+	59,
+	23,
+	1,
+	31,
+	12,
+	10
+};
+
+uint32_t	timeTrigs = 0;
+
+void 
+timeFunc()
+{
+	timeTrigs += 1;
+}
+
+void 
+setup()
+{
+	SPI.begin();
+	SPI.setBitOrder(MSBFIRST);
+	SPI.setDataMode(SPI_MODE1);
+	Serial.begin(9600);
+	delay(10);
+
+	RTC_init(chipSelect, intFreq);
+	setTime(chipSelect, &example_vals);
+	attachInterrupt(0, timeFunc, RISING);
+
+}
+
+void 
 loop()
 {
-	digitalWrite(PIN_LED_OUT, LED_ON);
-	delay(25);
+	delay(1000);
+	readTime(chipSelect, &example_vals);
 
-	digitalWrite(PIN_LED_OUT, LED_OFF);
-	delay(50);
+	Serial.print("Time: ");
+	Serial.print(example_vals.hh);
+	Serial.print(":");
+	Serial.print(example_vals.mm);
+	Serial.print(":");
+	Serial.println(example_vals.ss);
 
-	digitalWrite(PIN_LED_OUT, LED_ON);
-	delay(25);
+	Serial.print("Date: ");
+	Serial.print(example_vals.y);
+	Serial.print("/");
+	Serial.print(example_vals.m);
+	Serial.print("/");
+	Serial.println(example_vals.d);
 
-	digitalWrite(PIN_LED_OUT, LED_OFF);
-	delay(900);
+	Serial.print("Time Triggers: ");
+	Serial.println(timeTrigs);
+	timeTrigs = 0;
 }
